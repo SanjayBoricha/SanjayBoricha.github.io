00d6da0e20b3e358a6d37a5b2f25acc6c561e03b
Sanjay Boricha <sanjayboricha1001@gmail.com>
2022-08-27 09:33:45 +0530

    set target to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
-  ssr: false,
+  ssr: true,
   // Target: https://go.nuxtjs.dev/config-target
-  // target: 'static',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
