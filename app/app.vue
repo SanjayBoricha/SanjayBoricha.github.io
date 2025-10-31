a90855c598ad2f1301a1542504b5b4236d5979df
Sanjay Boricha <sanjayboricha1001@gmail.com>
2025-10-31 22:38:10 +0530

    WIP: updating design and nuxt

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="css">
+html,
+body {
+  font-family: "Inter", sans-serif;
+  font-optical-sizing: auto;
+  background-color: var(--color-gray-800);
+}
+</style>
